c9a57e33e3b945718f34151f06fb7358e310cf4d
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-06-08 22:01:30 +0200

    base classes

---
+++ src/main/java/cz/kocabek/AppConfig.java
@@ -0,0 +1,14 @@
+package cz.kocabek;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "cz.kocabek")
+public class AppConfig implements WebMvcConfigurer {
+
+
+}
