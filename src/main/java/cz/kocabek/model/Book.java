de5a2eb9df31b5fa18f399f250d7acb6ea332557
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-06-08 22:08:26 +0200

    bare Book class

---
+++ src/main/java/cz/kocabek/model/Book.java
@@ -0,0 +1,11 @@
+package cz.kocabek.model;
+
+public class Book {
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+
+}
